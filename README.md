d2b270e2c15f6ebed66165eae352ebd87e50c477
Mario Vidal Domínguez <Strikeroro@gmail.com>
2022-05-26 18:05:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python-Django---The-Practical-Guide
+About Repository of the Course from Udemy: https://www.udemy.com/course/python-django-the-practical-guide/
---
+++ django_course_site/meetups/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='all-meetups'),  # our-domain.com/meetups
+    path('<slug:meetup_slug>/success', views.confirm_registration,
+         name='confirm-registration'),
+    # our-domain.com/meetups/<dynamic-path-segment>
+    path('<slug:meetup_slug>',
+         views.meetup_details, name='meetup-detail')
+]
